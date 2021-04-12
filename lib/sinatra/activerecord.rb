4c8f5563c5635371e47e84e37940200fd75d903d
Joakim Carrilho <joakim.carrilho@klaxit.com>
2021-04-12 17:00:47 +0200

    Do not inject the config of DATABASE_URL if multiple base config

--- lib/sinatra/activerecord.rb
+++ lib/sinatra/activerecord.rb
@@ -30,9 +30,17 @@ module Sinatra
         else
           url_spec = ActiveRecord::ConnectionAdapters::ConnectionSpecification::ConnectionUrlResolver.new(url).to_hash
         end
-        
-        # url_spec will override the same key, if exist
-        final_spec = file_spec.keys.map{ |env| [env, file_spec[env].merge(url_spec)] }.to_h
+
+        # url_spec will override the same key, if exist, and if the
+        # configuration concerns only one database
+        final_spec = file_spec.keys.map do |env|
+          config = file_spec[env]
+          if config.is_a?(Hash) && config.all? { |_k, v| v.is_a?(Hash) }
+            [env, config]
+          else
+            [env, config.merge(url_spec)]
+          end
+        end.to_h
 
         app.set :database, final_spec
       elsif ENV['DATABASE_URL']
